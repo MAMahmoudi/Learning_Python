c10c01d02f3f2746cc917290f73663c3e9397343
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2025-12-22 13:49:01 +0100

    Starting Basics

---
+++ assets/JS/Menu_Items.js
@@ -0,0 +1,101 @@
+export const Menu_Items = [
+  {
+    title: "Home",
+    url: "index.html",
+    cName: "Nav_Bar_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Basics",
+    url: "Basics/Basics.html",
+    cName: "Nav_Bar_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Teachers",
+    url: "Teachers.html",
+    cName: "Nav_Bar_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Basics",
+    url: "Basics/Basics.html",
+    cName: "Nav_Bar_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Settings",
+    url: "https://www.ensia.edu.dz/",
+    cName: "Nav_Bar_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+];
+
+export const Labs_Items = [
+  {
+    title: "Lab 0: Team, project and GitHub account",
+    url: "Lab_0.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 1: Installations",
+    url: "Lab_1.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 2: Project structure",
+    url: "Lab_2.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 3: CSS part 2",
+    url: "Lab_3.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 4: Javascript form validation",
+    url: "Lab_4.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 5: MySQL",
+    url: "Lab_5.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 6: PDO",
+    url: "Lab_6.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 7: ",
+    url: "Lab_7.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 8: ",
+    url: "Lab_8.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 9: ",
+    url: "Lab_9.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+  {
+    title: "Lab 10: PHP part 3",
+    url: "Lab_10.html",
+    cName: "Labs_Menu_Item",
+    icon: "fa-solid fa-user-plus",
+  },
+];
